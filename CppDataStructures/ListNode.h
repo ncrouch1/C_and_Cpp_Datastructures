1176767b7467bbde15a4e85b9e2f899a0ab21638
ncrouch1 <bjoonho@outlook.com>
2023-03-15 23:16:26 -0700

    made some organization decisions as well as worked on CPP things

--- CppDataStructures/ListNode.h
+++ CppDataStructures/ListNode.h
@@ -2,8 +2,8 @@
 // Created by noah- on 3/14/2023.
 //
 
-#ifndef LINKEDLIST_H
-#define LINKEDLIST_H
+#ifndef ListNode_H
+#define ListNode_H
 
 #include <cstdlib>
 
@@ -15,6 +15,7 @@ public:
     ListNode(T data, ListNode<T>* next);
     ListNode(T data);
 };
+
 template <typename T>
 ListNode<T>::ListNode(T data) {
     this->data = data;
@@ -25,21 +26,5 @@ ListNode<T>::ListNode(T data, ListNode<T>* next) {
     this->data = data;
     this->next = next;
 }
-
-template <typename T>
-class LinkList {
-    ListNode<T> head;
-    int size;
-    public:
-        int add(T item);
-        int remove(int index);
-        void clear(void);
-        int getSize(void);
-        T get(int index);
-};
-
-template<typename T>
-
-
 
 #endif //LINKEDLIST_H
